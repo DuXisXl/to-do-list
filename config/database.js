0c6aa3410e4de18b52e51b73a76d6e1fa64b96a5
DuXisXl <eduardoramos245r@gmail.com>
2023-11-10 01:15:47 -0300

    Criação de Pagina para registro de atividdades

---
+++ config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1:27017/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Conectando ao mongoDB'))
+    .catch((err) => console.error(err));
---
+++ src/routes/checklist.js
@@ -0,0 +1,65 @@
+const express = require('express');
+
+const Checklist = require('../models/checklist');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    let checklist = await Checklist.find({});
+    res.status(200).render('checklists/index', { checklist: checklist });
+  } catch (error) {
+    res.status(200).render('pages/error', {error: 'Erro ao exibir as listas'});
+  }
+})
+
+router.get('/new', async (req, res) => {
+  try {
+    let checklist = new Checklist();
+    res.status(200).render('checklists/new', { checklist: checklist });
+  } catch (error) {
+    res.status(500).render('pages/error', {errors: 'Erro ao carregar o formulario'})
+  }
+})
+
+router.post('/', async (req, res) => {
+  let { name } = req.body.checklist;
+  let checklist = new Checklist({name})
+
+  try {
+    await checklist.save();
+    res.redirect('/checklists');
+  } catch (error) {
+    res.status(500).render('checklists/new', { checklists: { ...checklist, error }});
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).render('checklists/show', { checklist: checklist });
+  } catch (error) {
+    res.status(200).render('pages/error', {error: 'Não foi possível exibir as listas'});
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    let { name } = req.body
+    let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndDelete(req.params.id);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error)
+  }
+})
+
+module.exports = router;
